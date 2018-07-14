5d2f9b956e62c7185fcf2dda3b876e28fd561779
Darwin Do <darwindo@outlook.com>
2018-07-14 03:51:36 -0700

    showed message from backend

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('here')
+  res.send('BACKEND SERVER TO BE IMPLEMENTED')
 });
 
 app.listen(PORT, () => console.log('Server running on port', PORT));
